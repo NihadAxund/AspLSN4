f71095185b3a86d42e9271e6321ca6ce156311a5
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-04-27 16:11:08 +0400

    Add project files.

---
+++ AspLesson4/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace AspLesson4.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product()
+        {
+
+        }
+        public void Changing(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+
+        }
+    }
+}
---
+++ AspLesson4/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using AspLesson4.Entities;
+using AspLesson4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AspLesson4.Pages.Product
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProductDbContext _context;
+        private IndexModel() { }
+        public IndexModel(ProductDbContext context)
+        {
+            _context = context;
+        }
+        public string Message { get; set; }
+        public string Info { get; set; }
+        
+        public List<Entities.Product> Products { get; set; }
+
+        public static int Id_P = -1;
+        public void OnGet(string info = "", string value = "none", int OD = -1)
+        {
+        
+            Products = _context.Products.ToList();
+            Message = $"Today is {DateTime.Now.DayOfWeek}";
+            DisplayM = value;
+            if (OD != -1)
+            {
+                var data2 = _context.Products.FirstOrDefault(r => r.Id == OD);
+                Id_P = OD;
+                Product = data2;
+            }
+            else
+            {
+                Product = new Entities.Product();
+            }
+            Info = info;
+        }
+
+        [BindProperty]
+        public Entities.Product Product { get; set; }
+        [BindProperty]
+        public string DisplayM { get; set; } = "flex";
+        public IActionResult OnPost()
+        {
+            _context.Products.Add(Product);
+            _context.SaveChanges();
+            Info = $"{Product.Name} added successfully";
+            return RedirectToPage("Index",new { info=Info});
+        }
+
+        public IActionResult OnPostEditProdcut(int id)
+        {
+
+            return RedirectToPage("Index", new { info = Info,value = DisplayM,OD=id});
+        }
+
+        public IActionResult OnPostEdit_Product()
+        {
+         
+            var product2 = _context.Products.FirstOrDefault(p => p.Id == Id_P);
+            product2.Changing(Product);
+            _context.SaveChanges();
+            Info = $"{Product.Name} Edit successfully";
+
+            return RedirectToPage("Index", new { info = Info });
+        }
+
+        public IActionResult OnPostMyButton(int id)
+        {
+            if (id != null)
+            {
+                var recordToDelete = _context.Products.FirstOrDefault(r => r.Id == id);
+                if (recordToDelete != null)
+                {
+                    _context.Products.Remove(recordToDelete);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    return BadRequest("Not ID");
+                }
+                Info = $"{Product.Name} Deleted successfully";
+            }
+            return RedirectToPage("Index", new { info = Info });
+
+        }
+
+    }
+}
